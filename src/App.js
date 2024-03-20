8e19181bb5f3fa183ac502ab87a94f14d177a0b8
kenny <kenny.kwyiu@gmail.com>
2024-03-21 07:52:40 +0800

    190-created Main and Header components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import DateCounter from "./DateCounter";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
