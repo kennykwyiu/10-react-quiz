56bb4080601cd61790ec7174e2068cfe01f705fa
kenny <kenny.kwyiu@gmail.com>
2024-03-23 14:10:49 +0800

    191-fetching the data from the fake api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  // const [state, dispatch] = useReducer(initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error"));
+  }, []);
   return (
     <div className="app">
       <Header>The React Quiz</Header>
