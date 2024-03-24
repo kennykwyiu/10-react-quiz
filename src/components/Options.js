27f6a69382230c4088198353eec6166c2fde8704
kenny <kenny.kwyiu@gmail.com>
2024-03-25 01:00:47 +0800

    195-handle new answer by added newAnswer in reducer

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function Options({ question }) {
+function Options({ question, dispatch, answer }) {
   return (
     <div className="options">
-      {question.options.map((option) => (
-        <button className="btn btn-option" key={option}>
+      {question.options.map((option, index) => (
+        <button
+          className="btn btn-option"
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
           {option}
         </button>
       ))}
