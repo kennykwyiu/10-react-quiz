2caccb8a47ebc4a390372c0b8b8afa3c39d17c42
kenny <kenny.kwyiu@gmail.com>
2024-03-25 01:07:04 +0800

    195-added style on the answer

--- src/components/Options.js
+++ src/components/Options.js
@@ -5,8 +5,11 @@ function Options({ question, dispatch, answer }) {
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className="btn btn-option"
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            index === question.correctOption ? "correct" : "wrong"
+          }`}
           key={option}
+          disabled={answer !== null}
           onClick={() => dispatch({ type: "newAnswer", payload: index })}
         >
           {option}
