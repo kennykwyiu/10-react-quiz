564dd5558ec62fdb11bd875b91859b2873846de9
kenny <kenny.kwyiu@gmail.com>
2024-03-27 22:56:11 +0800

    197-make use answer to fix the progress bar

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-function Progress({ index, numQuestions, points, maxPossiblePoints }) {
+function Progress({ index, numQuestions, points, maxPossiblePoints, answer }) {
   return (
     <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
       <p>
         Question <strong>{index + 1}</strong> / {numQuestions}
       </p>
