d278c3acf3fa1f27623603878d4e38dec0c7de1b
kenny <kenny.kwyiu@gmail.com>
2024-03-29 11:58:32 +0800

    198-added emoji in the finishScreen

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function FinishScreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (percentage === 100) emoji = "👍";
+  if (percentage >= 80 && percentage < 100) emoji = "😍";
+  if (percentage >= 50 && percentage < 80) emoji = "💪";
+  if (percentage >= 0 && percentage < 50) emoji = "🤔🤔🤔";
+  if (percentage === 0) emoji = "😣";
+
   return (
     <p className="result">
-      You scored <strong>{points}</strong> out of {maxPossiblePoints} (
-      {Math.ceil(percentage)}%)
+      <span>{emoji}</span>You scored <strong>{points}</strong> out of{" "}
+      {maxPossiblePoints} ({Math.ceil(percentage)}%)
     </p>
   );
 }
